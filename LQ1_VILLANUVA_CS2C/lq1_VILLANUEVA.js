8324e031c38e1a8f481900091fae790b6cdfbb72
Villanueva, Robert G. <bertvillxxxxva@gmail.com>
2024-09-22 07:27:28 -0800

    Add files via upload

---
+++ LQ1_VILLANUVA_CS2C/lq1_VILLANUEVA.js
@@ -0,0 +1,18 @@
+// Persona 1
+const persona1Name = "Maria Delos Santos Las Marias";
+const persona1Age = 25;
+const persona1Address = "Upper Session Road, Baguio City, Benguet";
+
+// Persona 2
+const persona2Name = "Juan Gamoso Dela Cruz";
+const persona2Age = 28;
+const persona2Address = "San Nicolas, Candon City, Ilocos Sur";
+
+console.log(persona1Name)
+console.log(persona1Age)
+console.log(persona1Address)
+
+console.log(persona2Name)
+console.log(persona2Age)
+console.log(persona2Address)
+
